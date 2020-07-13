9dfc1889b74b8b42168f67c78aec01cf8ab6be78
Mihir <mihirchauhan756@gmail.com>
2020-07-13 20:18:23 +0530

    Intial

--- app/src/main/java/com/example/hackathon/handlers/DatabaseHandler.java
+++ app/src/main/java/com/example/hackathon/handlers/DatabaseHandler.java
@@ -1,50 +1,80 @@
 package com.example.hackathon.handlers;
 
+import android.content.Context;
 import android.provider.ContactsContract;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-public class DatabaseHandler {
+import java.util.HashMap;
+import java.util.Map;
 
-    public static DatabaseHandler object = null;
-    public static Response.ErrorListener errorListener = null;
-    public static Response.Listener responseListener = null;
+public abstract class DatabaseHandler {
 
-    public static DatabaseHandler getInstance(final String url){
+    Context context = null;
+    String Url = null;
+    Map<String,String> map = null;
+    RequestQueue requestQueue = null;
 
-        if(object==null)
-        {
-            object = new DatabaseHandler();
-
-            errorListener = new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.d("VolleyError", "StringRequestError: "+error);
-                }
-            };
-
-            responseListener =  new Response.Listener() {
-                @Override
-                public void onResponse(Object response) {
-                    getResponse(url);
-                }
-            };
-
-            return object;
-        }
-        else
-        {
-            return object;
-        }
-    }
-
-    public static void getResponse(String url)
+    public DatabaseHandler(Context context, String url)
     {
-        StringRequest stringRequest = new StringRequest(StringRequest.Method.POST,url,responseListener,errorListener);
-
+        this.context = context ;
+        this.Url = url;
     }
+
+    public abstract void getResponse(String response);
+
+    public void putValues(Map<String,String> map){
+         this.map = map;
+    }
+
+    public void execute(){
+        requestQueue = Volley.newRequestQueue(context);
+        StringRequest request = new StringRequest(Request.Method.POST, Url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        getResponse(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("VolleyError", "onErrorResponse: "+error);
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return map;
+            }
+        };
+
+        request.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 100;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 100;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
+
+        requestQueue.add(request);
+    };
+
+
+
 }
