a736a8a52d1285e9f6607652c4f43860a1fe2d2e
Dev-karanraj <rajkaran5478@gmail.com>
2020-07-13 17:51:00 +0530

    Login Layout

---
+++ app/src/main/java/com/example/hackathon/SignupActivity.java
@@ -0,0 +1,14 @@
+package com.example.hackathon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+    }
+}
