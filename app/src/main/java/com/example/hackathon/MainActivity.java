3118e406e0dbbb5000e4a01dd4f922b141998c98
Karan <rajkaran5478@gmail.com>
2020-07-14 10:12:52 +0530

    New Color

--- app/src/main/java/com/example/hackathon/MainActivity.java
+++ app/src/main/java/com/example/hackathon/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.hackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                gotoNextActivity();
+
+            }
+        },500);
+    }
+
+    private void gotoNextActivity()
+    {
+        intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
