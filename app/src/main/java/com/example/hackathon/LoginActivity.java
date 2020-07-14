f5d0270f11e185b4382ddaf7dfb4f7a7c199612a
Mihir <mihirchauhan756@gmail.com>
2020-07-14 10:39:05 +0530

    Mihir Side

--- app/src/main/java/com/example/hackathon/LoginActivity.java
+++ app/src/main/java/com/example/hackathon/LoginActivity.java
@@ -47,7 +47,6 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 loginOperation();
             }
         });
--- app/src/main/java/com/example/hackathon/MainActivity.java
+++ app/src/main/java/com/example/hackathon/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
